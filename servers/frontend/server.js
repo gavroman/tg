eaffdd4f2f6fce7d77ee4408d29187c32da3e0e7
gavroman <gavroman1998@gmail.com>
2020-05-06 12:03:24 +0300

    fix

--- servers/frontend/server.js
+++ servers/frontend/server.js
@@ -8,6 +8,8 @@ const distFolder = path.resolve(__dirname, '../', '../', 'dist');
 const imgFolder = path.resolve(__dirname, '../', '../', 'img');
 app.use(express.static(distFolder));
 app.use(express.static(imgFolder));
+app.use(morgan('dev'));
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(distFolder, 'index.html'));
